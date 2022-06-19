670eaa0ffbe8cfe2999dd0e9cc86eb9dfadf5ca2
Artem Baranov <temoncher@yandex.ru>
2022-06-20 01:54:06 +0300

    implement tile groups, improve drag experience

--- src/Scene.tsx
+++ src/Scene.tsx
@@ -25,22 +25,100 @@ import {
 softShadows();
 
 enum TerrainType {
-  WOODS = 'WOODS',
+  FOREST = 'FOREST',
   FIELDS = 'FIELDS',
+  SETTLEMENT = 'SETTLEMENT',
+  WATER = 'WATER',
 }
+
+const explorations: Exploration[] = [
+  {
+    name: 'Land',
+    type: TerrainType.FIELDS,
+    mask: [
+      [0, 1, 0, 0],
+      [1, 1, 1, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+  {
+    name: 'Forgotten forest',
+    type: TerrainType.FOREST,
+    mask: [
+      [0, 1, 0, 0],
+      [0, 1, 1, 0],
+      [0, 0, 1, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+  {
+    name: 'Farm',
+    type: TerrainType.FIELDS,
+    mask: [
+      [0, 1, 0, 0],
+      [0, 1, 1, 0],
+      [0, 1, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+  {
+    name: 'Town',
+    type: TerrainType.SETTLEMENT,
+    mask: [
+      [1, 1, 1, 0],
+      [1, 1, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+  {
+    name: 'Finshing village',
+    type: TerrainType.SETTLEMENT,
+    mask: [
+      [1, 1, 1, 1],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+  {
+    name: 'River in the fields',
+    type: TerrainType.WATER,
+    mask: [
+      [1, 1, 1, 0],
+      [1, 0, 0, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+  {
+    name: 'Swamp',
+    type: TerrainType.WATER,
+    mask: [
+      [1, 0, 0, 0],
+      [1, 1, 1, 0],
+      [1, 0, 0, 0],
+      [0, 0, 0, 0],
+    ],
+  },
+];
+
 type Exploration = {
+  name: string;
   type: TerrainType;
+  mask: (0 | 1)[][];
 };
 
 const terrainTypeToColorMap = {
-  [TerrainType.FIELDS]: 'yellow',
-  [TerrainType.WOODS]: 'green',
+  [TerrainType.FIELDS]: 'gold',
+  [TerrainType.FOREST]: 'forestgreen',
+  [TerrainType.SETTLEMENT]: 'maroon',
+  [TerrainType.WATER]: 'royalblue',
 };
 
-function getRandomEnumMember<T extends string | number, TEnumValue extends string>(enumVariable: { [key in T]: TEnumValue }) {
-  const enumValues = Object.values(enumVariable);
-
-  return enumValues[Math.floor(Math.random() * enumValues.length)] as TEnumValue;
+function getRandomIndex(length: number) {
+  return Math.floor(Math.random() * length);
 }
 
 function isTouchDevice() {
@@ -49,20 +127,35 @@ function isTouchDevice() {
 
 function applyExploration([rowIndex, columnIndex]: Vector2Tuple, exploration: Exploration) {
   return (tileRows: (TerrainType | undefined)[][]) => {
-    const updatedTileRows = [...tileRows];
+    const updatedTileRows = tileRows.map((row) => [...row]);
 
-    updatedTileRows[rowIndex]![columnIndex] = exploration.type;
+    const halfRowSize = Math.floor(exploration.mask.length / 2);
+
+    for (let currentRowIndex = 0; currentRowIndex < exploration.mask.length; currentRowIndex++) {
+      const maskRow = exploration.mask[currentRowIndex]!;
+      const halfColumnSize = Math.floor(maskRow.length / 2);
+
+      for (let currentColumnIndex = 0; currentColumnIndex < maskRow.length; currentColumnIndex++) {
+        const maskColumn = maskRow[currentColumnIndex]!;
+
+        if (maskColumn) {
+          updatedTileRows[currentRowIndex + rowIndex - halfRowSize]![currentColumnIndex + columnIndex - halfColumnSize] = exploration.type;
+        }
+      }
+    }
 
     return updatedTileRows;
   };
 }
 
+const MOBILE_TOUCH_OFFSET = 2;
+
 function adjustRayOriginForMobile(ray: Ray) {
   const newRay = ray.clone();
 
   if (isTouchDevice()) {
-    newRay.origin.x = newRay.origin.x - MOBILE_TOUCH_OFFSET * 2;
-    newRay.origin.x = newRay.origin.x + MOBILE_TOUCH_OFFSET;
+    newRay.origin.x = newRay.origin.x - MOBILE_TOUCH_OFFSET;
+    newRay.origin.z = newRay.origin.z - MOBILE_TOUCH_OFFSET;
   }
 
   return newRay;
@@ -73,23 +166,34 @@ const emptyRows: (TerrainType | undefined)[][] = Array.from({ length: 11 }, () =
 const gridCenter = new Vector3(9, 0, 1);
 
 export default function Scene() {
+  const [gridIsVisible, setGridVisibility] = useState(false);
   const [tileRows, setTileRows] = useState(() => emptyRows);
   const gridRef = useRef<Mesh<BufferGeometry, Material>>(null);
 
   return (
-    <Canvas style={{ touchAction: 'none' }}>
-      {/* <primitive object={new AxesHelper(15)} /> */}
-      <Camera />
-      <Lights />
-      <Grid gridRef={gridRef} rows={tileRows} position={gridCenter.clone().setY(0.2).toArray()} />
-      <Ground size={tileRows.length} position={gridCenter.toArray()} />
-      <ExplorationHub
-        gridRef={gridRef}
-        onGridDrop={(insertAt, exploration) => {
-          setTileRows(applyExploration(insertAt, exploration));
+    <>
+      <button
+        style={{ position: 'absolute', padding: '0.5rem 1rem', zIndex: 10 }}
+        onClick={() => {
+          setGridVisibility(!gridIsVisible);
         }}
-      />
-    </Canvas>
+      >
+        Toggle grid
+      </button>
+      <Canvas style={{ touchAction: 'none' }}>
+        {/* <primitive object={new AxesHelper(15)} /> */}
+        <Camera />
+        <Lights />
+        <Grid gridIsEnabled={gridIsVisible} gridRef={gridRef} rows={tileRows} position={gridCenter.clone().setY(0.2).toArray()} />
+        <Ground size={tileRows.length} position={gridCenter.toArray()} />
+        <ExplorationHub
+          gridRef={gridRef}
+          onGridDrop={(insertAt, exploration) => {
+            setTileRows(applyExploration(insertAt, exploration));
+          }}
+        />
+      </Canvas>
+    </>
   );
 }
 
@@ -105,7 +209,14 @@ function Camera() {
   return <OrthographicCamera makeDefault zoom={25} />;
 }
 
-function Grid(props: { rows: (TerrainType | undefined)[][]; position: Vector3Tuple; gridRef: React.RefObject<Mesh<BufferGeometry, Material>> }) {
+type GridProps = {
+  gridIsEnabled: boolean;
+  rows: (TerrainType | undefined)[][];
+  position: Vector3Tuple;
+  gridRef: React.RefObject<Mesh<BufferGeometry, Material>>;
+};
+
+function Grid(props: GridProps) {
   const [gridX, gridY, gridZ] = props.position;
   const halfGridSize = Math.floor(props.rows.length / 2);
   const gridCenterPos: Vector3Tuple = [halfGridSize - gridX, gridY, halfGridSize - gridZ];
@@ -116,7 +227,7 @@ function Grid(props: { rows: (TerrainType | undefined)[][]; position: Vector3Tup
         <planeBufferGeometry attach="geometry" args={[props.rows.length, props.rows.length]} />
         <meshBasicMaterial attach="material" side={DoubleSide} visible={false} />
       </mesh>
-      {/* <gridHelper args={[props.rows.length, props.rows.length, 'blue', 'blue']} position={[0, 0.1, 0]} /> */}
+      {props.gridIsEnabled && <gridHelper args={[props.rows.length, props.rows.length, 'blue', 'blue']} position={[0, 0.1, 0]} />}
 
       {props.rows.map((row, x) =>
         row.map((terrainType, z) =>
@@ -146,7 +257,6 @@ function Lights() {
 }
 
 const defaultExplorationTilePosition = new Vector3(10, 0.4, -5);
-const MOBILE_TOUCH_OFFSET = 2;
 
 const horizontalPlane = new Plane(new Vector3(0, 1, 0));
 
@@ -156,8 +266,8 @@ type ExplorationHubProps = {
 };
 
 function ExplorationHub(props: ExplorationHubProps) {
-  const [currentExploration, setCurrentExploration] = useState<Exploration>(() => ({ type: TerrainType.WOODS }));
-  const [springPos, api] = useSpring(() => ({ position: defaultExplorationTilePosition.toArray(), visible: true }));
+  const [currentExploration, setCurrentExploration] = useState<Exploration>(() => explorations[getRandomIndex(explorations.length)]!);
+  const [springPos, api] = useSpring(() => ({ position: defaultExplorationTilePosition.toArray() }));
   const bind = useDrag(({ down, event }) => {
     const adjustedRay = adjustRayOriginForMobile((event as unknown as { intersections: Intersection[] }).ray);
 
@@ -180,14 +290,10 @@ function ExplorationHub(props: ExplorationHubProps) {
       }
     } else {
       if (gridIntersection) {
-        api.start({ visible: false });
-
         const sum = new Vector3().addVectors(gridIntersection.clone().round(), gridCenter);
 
         props.onGridDrop([sum.x, sum.z], currentExploration);
-        setCurrentExploration({
-          type: getRandomEnumMember(TerrainType),
-        });
+        setCurrentExploration({ ...explorations[getRandomIndex(explorations.length)]! });
       } else {
         api.start({ position: defaultExplorationTilePosition.toArray() });
       }
@@ -195,14 +301,25 @@ function ExplorationHub(props: ExplorationHubProps) {
   });
 
   useEffect(() => {
-    api.start({ immediate: true, visible: true, position: defaultExplorationTilePosition.toArray() });
+    api.start({ immediate: true, position: defaultExplorationTilePosition.toArray() });
   }, [currentExploration]);
 
+  const halfGridSize = Math.floor(currentExploration.mask.length / 2);
+
   return (
-    <>
-      {/* @ts-expect-error bind type mismatch */}
-      <Tile {...springPos} {...bind()} color={terrainTypeToColorMap[currentExploration.type]} />
-    </>
+    <a.group {...springPos} {...bind()}>
+      <mesh ref={props.gridRef} rotation={[-Math.PI / 2, 0, 0]} position={[0, 0.1, 0]}>
+        <planeBufferGeometry attach="geometry" args={[currentExploration.mask.length, currentExploration.mask.length]} />
+        <meshBasicMaterial attach="material" side={DoubleSide} visible={false} />
+      </mesh>
+      {currentExploration.mask.map((row, x) =>
+        row.map((hasTile, z) =>
+          hasTile === 0 ? null : (
+            <Tile key={`(${x},${z})`} color={terrainTypeToColorMap[currentExploration.type]} position={[x - halfGridSize, 0.1, z - halfGridSize]} />
+          )
+        )
+      )}
+    </a.group>
   );
 }
 
